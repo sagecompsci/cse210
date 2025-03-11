4dfdb1df1513737755d816fc0a9f3f8a16a51bec
sagecompscie <sagereid32@gmail.com>
2025-03-11 14:45:10 -0600

    resume program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Apple";
+        job1._startYear = 2000;
+        job1._endYear = 2005;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Backend Developer";
+        job2._company = "Netflix";
+        job2._startYear = 2003;
+        job2._endYear = 2010;
+
+        Resume resume = new Resume();
+        resume._name = "Alan Turing";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
     }
+    
 }
