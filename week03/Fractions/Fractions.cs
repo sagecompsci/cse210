cc79fd64a7591e178157713018d725f64012e1d3
sagecompscie <sagereid32@gmail.com>
2025-03-19 00:05:27 -0600

    fraction program

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,53 @@
+﻿public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+    public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fractions(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public float GetTop()
+    {
+        return _top;
+    }
+
+    public float GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetString()
+    {
+        return $"{GetTop()}/{GetBottom()}";
+    }
+
+    public float GetDecimal()
+    {
+        return GetTop() / GetBottom();
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions f1 = new Fractions();
+        Fractions f2 = new Fractions(6);
+        Fractions f3 = new Fractions(6, 7);
+        
+        f1.SetTop(2);
+        f1.SetBottom(3);
+        f2.SetTop(4);
+        f2.SetBottom(5);
+        f3.SetTop(6);
+        f3.SetBottom(7);
+        
+        Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
+        Console.WriteLine($"{f2.GetTop()}/{f2.GetBottom()}");
+        Console.WriteLine($"{f3.GetTop()}/{f3.GetBottom()}");
+        Console.WriteLine("");
+        
+        Console.WriteLine(f1.GetString());
+        Console.WriteLine(f1.GetDecimal());
+        Console.WriteLine(f2.GetString());
+        Console.WriteLine(f2.GetDecimal());
+        Console.WriteLine(f3.GetString());
+        Console.WriteLine(f3.GetDecimal());
+        
     }
 }
