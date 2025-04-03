4c8e9aa30b218abf7439b9a7a3c50149ef48230e
sagecompscie <sagereid32@gmail.com>
2025-04-03 01:58:12 -0600

    inheritance learning

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,17 @@
+﻿public class MathAssignment : Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textBook, string problems) 
+        : base(name, topic)
+    {
+        _textBookSection = textBook;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textBookSection} Problems {_problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Jack Harkness", "Web Development");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("");
+        
+        MathAssignment mathAssignment = new MathAssignment("John Smith", "Fractions", "4.1", "1-13");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Clara Oswald", "Civil War", "Why War Is Bad");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
